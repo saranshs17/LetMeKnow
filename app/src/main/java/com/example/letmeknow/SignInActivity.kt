9897afcecd900886ca2c7d25c1a732434656b80f
SARANSH <saranshmain17@gmail.com>
2023-03-04 06:57:03 +0530

    Final Commit

---
+++ app/src/main/java/com/example/letmeknow/SignInActivity.kt
@@ -0,0 +1,41 @@
+package com.example.letmeknow
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.letmeknow.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth=FirebaseAuth.getInstance()
+        binding.directsignup.setOnClickListener{
+            val intent= Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.signinButton.setOnClickListener{
+            val email = binding.signinEmail.text.toString()
+            val pass = binding.signinPass.text.toString()
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                    firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                Toast.makeText(this, "Empty Fields are not allowed!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/letmeknow/SignUpActivity.kt
@@ -0,0 +1,46 @@
+package com.example.letmeknow
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.letmeknow.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.directsignin.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        binding.signupButton.setOnClickListener {
+            val email = binding.signupEmail.text.toString()
+            val pass = binding.signupPass.text.toString()
+            val repass = binding.signupRepass.text.toString()
+            if (email.isNotEmpty() && pass.isNotEmpty()&& repass.isNotEmpty()) {
+                if (pass == repass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, SignInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields are not allowed!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
