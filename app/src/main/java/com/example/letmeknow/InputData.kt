67437cea5de9dd3d996220628ae9b5fa295e570e
SARANSH <saranshmain17@gmail.com>
2023-03-15 18:16:24 +0530

    Second Commit

--- app/src/main/java/com/example/letmeknow/InputData.kt
+++ app/src/main/java/com/example/letmeknow/InputData.kt
@@ -3,4 +3,4 @@ package com.example.letmeknow
 data class InputData(
     val Question:String?=null,
     val DateandTime :String?=null
-    )
+)
--- app/src/main/java/com/example/letmeknow/SplashActivity.kt
+++ app/src/main/java/com/example/letmeknow/SplashActivity.kt
@@ -19,7 +19,7 @@ class SplashActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_splash)
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
             finish()
         },
